bf9d6bd58211f12c8ced12154d1fc2a9d9276c53
Debdeep <debdeepsarkar2jr@gmail.com>
2026-01-28 23:41:12 +0530

    first_NonRepetatingChar

--- src/Basics/first_NonRepetatingChar.java
+++ src/Basics/first_NonRepetatingChar.java
@@ -1,10 +1,23 @@
 package Basics;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class first_NonRepetatingChar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String str = "aiutomtion";
+		Set<Character> s = new HashSet<>();
+		char[] arr = str.toCharArray();
+		
+		for (char c : arr) {
+			if(!s.add(c)) {
+				System.out.println(c);
+				break;
+			}
+		}
+		
 	}
 
 }
